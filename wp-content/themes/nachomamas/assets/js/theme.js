576aec282abb68dda2344767526b50fa711ccdc8
Derek <derek@Dereks-iMac.local>
2016-06-07 12:52:18 -0400

    mobile edits

--- wp-content/themes/nachomamas/assets/js/theme.js
+++ wp-content/themes/nachomamas/assets/js/theme.js
@@ -1,56 +1,58 @@
   	$(document).ready(function(){
+      var headerHeightMobile = $(".mobile-logo").height();
+      var bannerHeight = $(".banner-image").height();
+      var Headheight = document.getElementById("header").offsetHeight;
+      var homeItemHeight = $(".home-image").height();
 
-  		$('#mobile-menu .menu').slicknav({
-  			label: '',
-  			prependTo: '#mobile-menu-location',
-  			duration: '1000',
-  			label: 'Menu',
-  		});
+      $('#mobile-menu .menu').slicknav({
+        label: '',
+        prependTo: '#mobile-menu-location',
+        duration: '1000',
+        label: 'Menu',
+      });
 
-      var headerHeight = $(".mobile-logo").height();
-        $(".slicknav_btn").height(headerHeight);
+      $(".slicknav_btn").height(headerHeightMobile);
 
-  	});
-
-  	$(window).load(function(){
-        if ($(window).width() < 700) {
-          var bannerHeight = $(".banner-image").height();
-            $(".main-banner").height(bannerHeight);
+      if ($(window).width() < 700) {
+        $(".main-banner").height(bannerHeight);
       }
+
       if ($(window).width() > 700) {
-        var Headheight = document.getElementById("header").offsetHeight;
         document.getElementById("banner-overlay").style.paddingTop = Headheight + 'px';
         document.getElementById("banner-overlay").style.paddingBottom = Headheight + 'px';
-        var homeItemHeight = $(".home-image").height();
-        var bannerHeight = $(".banner-image").height();
-          $(".home-content").height(homeItemHeight);
-          $(".main-banner").height(bannerHeight);
+        $(".home-content").height(homeItemHeight);
+        $(".main-banner").height(bannerHeight);
       }
-      if ($(window).width() < 1140) {
+
+      if ($(window).width() < 1122) {
           document.getElementById("banner-overlay").style.paddingBottom =  '0px';
       }
+
   	});
 
 		$(window).resize(function(){
+      var headerHeightMobile = $(".mobile-logo").height();
+      var bannerHeight = $(".banner-image").height();
+      var Headheight = document.getElementById("header").offsetHeight;
+      var homeItemHeight = $(".home-image").height();
+      var headerHeight2 = $(".mobile-logo").height();
+
+      $(".slicknav_btn").height(headerHeight2);
+
       if ($(window).width() < 700) {
         $(".home-content").height('auto');
-        var bannerHeight = $(".banner-image").height();
-          $(".main-banner").height(bannerHeight);
+        $(".main-banner").height(bannerHeight);
       }
+
       if ($(window).width() > 700) {
-        var homeItemHeight = $(".home-image").height();
-          $(".home-content").height(homeItemHeight);
-          var Headheight = document.getElementById("header").offsetHeight;
           document.getElementById("banner-overlay").style.paddingTop = Headheight + 'px';
           document.getElementById("banner-overlay").style.paddingBottom = Headheight + 'px';
-          var bannerHeight = $(".banner-image").height();
             $(".home-content").height(homeItemHeight);
             $(".main-banner").height(bannerHeight);
       }
-        if ($(window).width() < 1140) {
-            document.getElementById("banner-overlay").style.paddingBottom =  '0px';
-        }
-      var headerHeight2 = $(".mobile-logo").height();
-        $(".slicknav_btn").height(headerHeight2);
+
+      if ($(window).width() < 1122) {
+          document.getElementById("banner-overlay").style.paddingBottom =  '0px';
+      }
 
 		});
--- wp-content/themes/nachomamas/template-parts/content-home.php
+++ wp-content/themes/nachomamas/template-parts/content-home.php
@@ -9,27 +9,25 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
 	<div class="content-container">
+
 		<div class="content-title">
+
 			<header class="entry-header">
+
         <h1 class="entry-title">A true Baltimore institution</h1>
-			</header><!-- .entry-header -->
+
+			</header>
+
 		</div>
 
-
-
 		<div class="home-content">
-			<?php
-			// the_content();
-
-
-
-			?>
 
 			<?php get_template_part( 'template-parts/homepage-items' ); ?>
 
-		</div><!-- .entry-content -->
+		</div>
 
 	</div>
 
-</article><!-- #post-## -->
+</article>

413ee9c6cf4c7cd62f7e546d2cc49a727883c17d
Derek <derek@Dereks-iMac.local>
2016-06-24 02:52:55 -0400

    equal height js

--- wp-content/themes/nachomamas/assets/js/theme.js
+++ wp-content/themes/nachomamas/assets/js/theme.js
@@ -36,7 +36,7 @@ var currentTallest = 0,
       var Headheight = document.getElementById("header").offsetHeight;
       var homeItemHeight = $(".home-image").height();
 
-      equalheight('.event-item-wrapper');
+      equalheight('.event-item');
 
       $('#mobile-menu .menu').slicknav({
         label: '',
@@ -70,7 +70,7 @@ var currentTallest = 0,
       var Headheight = document.getElementById("header").offsetHeight;
       var homeItemHeight = $(".home-image").height();
       var headerHeight2 = $(".mobile-logo").height();
-      equalheight('.event-item-wrapper');
+      equalheight('.event-item');
 
       $(".slicknav_btn").height(headerHeight2);
 

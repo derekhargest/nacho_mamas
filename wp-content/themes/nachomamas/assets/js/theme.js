a20c9e42892b22ce18bfe07869e96a64e3fd4354
Derek <derek@Dereks-iMac.local>
2016-06-24 02:51:11 -0400

    equal height js

--- wp-content/themes/nachomamas/assets/js/theme.js
+++ wp-content/themes/nachomamas/assets/js/theme.js
@@ -1,8 +1,42 @@
-  	$(window).load(function(){
+equalheight = function(container){
+
+var currentTallest = 0,
+     currentRowStart = 0,
+     rowDivs = new Array(),
+     $el,
+     topPosition = 0;
+ $(container).each(function() {
+
+   $el = $(this);
+   $($el).height('auto')
+   topPostion = $el.position().top;
+
+   if (currentRowStart != topPostion) {
+     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+       rowDivs[currentDiv].height(currentTallest);
+     }
+     rowDivs.length = 0; // empty the array
+     currentRowStart = topPostion;
+     currentTallest = $el.height();
+     rowDivs.push($el);
+   } else {
+     rowDivs.push($el);
+     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+  }
+   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+     rowDivs[currentDiv].height(currentTallest);
+   }
+ });
+}
+
+
+    $(window).load(function(){
       var headerHeightMobile = $(".mobile-logo").height();
       var bannerHeight = $(".banner-image").height();
       var Headheight = document.getElementById("header").offsetHeight;
       var homeItemHeight = $(".home-image").height();
+
+      equalheight('.event-item-wrapper');
 
       $('#mobile-menu .menu').slicknav({
         label: '',
@@ -36,6 +70,7 @@
       var Headheight = document.getElementById("header").offsetHeight;
       var homeItemHeight = $(".home-image").height();
       var headerHeight2 = $(".mobile-logo").height();
+      equalheight('.event-item-wrapper');
 
       $(".slicknav_btn").height(headerHeight2);
 

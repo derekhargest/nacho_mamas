56aa091e58920a8ab4e1c2be06899d53fd0e567c
Derek <derek@Dereks-iMac.local>
2016-07-08 13:48:20 -0400

    replacing equal height

--- wp-content/themes/nachomamas/assets/js/theme.js
+++ wp-content/themes/nachomamas/assets/js/theme.js
@@ -36,6 +36,8 @@ var currentTallest = 0,
       var Headheight = document.getElementById("header").offsetHeight;
       var homeItemHeight = $(".home-image").height();
 
+      equalheight('.event-item');
+
 
       $('#mobile-menu .menu').slicknav({
         label: '',
@@ -69,6 +71,8 @@ var currentTallest = 0,
       var Headheight = document.getElementById("header").offsetHeight;
       var homeItemHeight = $(".home-image").height();
       var headerHeight2 = $(".mobile-logo").height();
+
+       equalheight('.event-item');
 
 
       $(".slicknav_btn").height(headerHeight2);
